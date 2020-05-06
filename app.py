f672a3ff6248b74b21bf185ea0672356b86cf3a6
Celia Garcia <cg.marquez95@gmail.com>
2020-05-06 14:46:26 +0200

    Modificaciones,avance y diseño

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template,request,abort
-app = Flask(__name__)	
+from flask import Flask,render_template,abort
+import json
+app = Flask(__name__)
 
-
-@app.route('/',methods=["GET"])
+@app.route('/')
 def inicio():
 	return render_template("inicio.html")
 
